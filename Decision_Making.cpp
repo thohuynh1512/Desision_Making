109bed6395c4dda157e7d59438b32146d7252b3b
thohuynh1512 <thohuynh1512@gmail.com>
2019-05-25 18:13:01 +0700

    fisrt commit

---
+++ Decision_Making.cpp
@@ -0,0 +1,18 @@
+﻿//Nhập vào 1 số nguyên, yêu cầu xuất ra chữ số hàng trăm của số đó, nếu không có thì xuất ra 0
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, so_chu_so = 0;
+	cout << "Nhap vao 1 so nguyen: ";
+	cin >> n;
+	if (n >= 100) {
+		cout << " chu so hang tram cua so " << n << " la " << n / 100 % 10 << endl;
+	}
+	else
+	{
+		cout << "0" << endl;
+	}
+	return 0;
+}
